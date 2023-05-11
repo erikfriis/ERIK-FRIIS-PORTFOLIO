1a43bec3b20f03b3d942c65c051e1377bf274c48
Erik Friis <erik.friis@hyperisland.sem>
2023-05-11 23:49:19 +0200

    responsivenss

--- src/components/sections/hero/Header.jsx
+++ src/components/sections/hero/Header.jsx
@@ -3,9 +3,11 @@ import HeaderCss from "./Header.module.css";
 const Header = () => {
 	return (
 		<div className={HeaderCss.container}>
-			<h1 className={HeaderCss.headingMain}>Exploring tools to innovate</h1>
-			<h1 className={`${HeaderCss.headingMain} ${HeaderCss.headerTwo}`}>
-				Frontend developer with background in music production
+			<h1 className={HeaderCss.headingMain}>
+				Exploring tools to innovate <br />
+				<span className={HeaderCss.headerSecondRow}>
+					Frontend developer with background in music production
+				</span>
 			</h1>
 		</div>
 	);
